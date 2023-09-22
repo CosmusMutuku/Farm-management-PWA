768e4d4224862bd27a84550fa8f6e3c1d60e8ab3
Cosmus Kavithi Mutuku <100933366+CosmusMutuku@users.noreply.github.com>
2023-09-22 14:18:49 +0300

    Updated app.js

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const port = process.env.PORT || 3001;
 // Middleware for parsing JSON data
 app.use(express.json());
 
+
 // Define your API routes and controllers here
 // For example: app.use('/api/daily-records', require('./routes/dailyRecords'));
 
